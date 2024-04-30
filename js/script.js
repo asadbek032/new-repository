506cd24a8831046ac0aa55b05112553ab5923f79
asad <newproject@gmail.com>
2024-04-30 23:53:12 +0500

    example

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"Use strict"
+
+// const plase = confirm("Aru you here");
+
+
+// const currentPlaxse = prompt("Where are you?", "Asad");
+
+const client = prompt("hello")
+console.log(client);
+
+// const channel = prompt("what is your favourite youtuber"); 
+
+// console.log("https://youtube.com" + channel + "/videos");
